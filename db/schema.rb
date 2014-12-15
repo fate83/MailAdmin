14b89a7fc6cb12d5adf6e3c880633a469520778c
Fabian Müller <fmueller83@googlemail.com>
2014-12-15 17:19:49 +0100

    updates schema for mysql

--- db/schema.rb
+++ db/schema.rb
@@ -14,61 +14,65 @@
 ActiveRecord::Schema.define(version: 20141214211625) do
 
   create_table "admins", force: true do |t|
-    t.string   "email",                  default: "",    null: false
-    t.string   "encrypted_password",     default: "",    null: false
-    t.string   "reset_password_token"
+    t.string   "email",                  limit: 255, default: "",    null: false
+    t.string   "encrypted_password",     limit: 255, default: "",    null: false
+    t.string   "reset_password_token",   limit: 255
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,     null: false
+    t.integer  "sign_in_count",          limit: 4,   default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.boolean  "admin",                  default: false
+    t.string   "current_sign_in_ip",     limit: 255
+    t.string   "last_sign_in_ip",        limit: 255
+    t.boolean  "admin",                  limit: 1,   default: false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  add_index "admins", ["email"], name: "index_admins_on_email", unique: true
-  add_index "admins", ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  add_index "admins", ["email"], name: "index_admins_on_email", unique: true, using: :btree
+  add_index "admins", ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true, using: :btree
 
   create_table "aliases", force: true do |t|
-    t.string   "user_source"
-    t.integer  "domain_source_id"
-    t.integer  "user_destination_id"
-    t.integer  "domain_destination_id"
-    t.datetime "created_at",            null: false
-    t.datetime "updated_at",            null: false
+    t.string   "user_source",           limit: 255
+    t.integer  "domain_source_id",      limit: 4
+    t.integer  "user_destination_id",   limit: 4
+    t.integer  "domain_destination_id", limit: 4
+    t.datetime "created_at",                        null: false
+    t.datetime "updated_at",                        null: false
   end
 
   create_table "domains", force: true do |t|
-    t.string   "name"
-    t.integer  "admin_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "name",       limit: 255
+    t.integer  "admin_id",   limit: 4
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
   end
 
-  add_index "domains", ["admin_id"], name: "index_domains_on_admin_id"
+  add_index "domains", ["admin_id"], name: "index_domains_on_admin_id", using: :btree
 
   create_table "forwardings", force: true do |t|
-    t.integer  "user_id"
-    t.integer  "domain_id"
-    t.string   "destination"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.integer  "user_id",     limit: 4
+    t.integer  "domain_id",   limit: 4
+    t.string   "destination", limit: 255
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
   end
 
-  add_index "forwardings", ["domain_id"], name: "index_forwardings_on_domain_id"
-  add_index "forwardings", ["user_id"], name: "index_forwardings_on_user_id"
+  add_index "forwardings", ["domain_id"], name: "index_forwardings_on_domain_id", using: :btree
+  add_index "forwardings", ["user_id"], name: "index_forwardings_on_user_id", using: :btree
 
   create_table "users", force: true do |t|
-    t.integer  "domain_id"
-    t.string   "username"
-    t.string   "password"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.integer  "domain_id",  limit: 4
+    t.string   "username",   limit: 255
+    t.string   "password",   limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
   end
 
-  add_index "users", ["domain_id"], name: "index_users_on_domain_id"
+  add_index "users", ["domain_id"], name: "index_users_on_domain_id", using: :btree
 
+  add_foreign_key "domains", "admins"
+  add_foreign_key "forwardings", "domains"
+  add_foreign_key "forwardings", "users"
+  add_foreign_key "users", "domains"
 end
