2d0de67ac419f3abb9a144cbbf96b0e8754511fe
Fabian Müller <fmueller83@googlemail.com>
2014-12-15 13:50:57 +0100

    encrypts password for users before save

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,13 @@
 class User < ActiveRecord::Base
+  before_save :encrypt_password
+
   belongs_to :domain
   has_many :aliases, foreign_key: :user_destination_id
   has_many :forwardings
+
+  private
+    def encrypt_password
+      `yes "#{self.password}" | doveadm pw -s SHA512-CRYPT` =~ /\{SHA512-CRYPT\}(.*)/
+      self.password = $1
+    end
 end
